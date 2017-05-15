9767a21fc4bcb528634ed9db232ff91b12938414
Hesham Mohamed <Lehkeda@users.noreply.github.com>
2017-05-15 21:47:08 +0200

    use prebuillt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,8 +31,9 @@ BOARD_KERNEL_PAGESIZE := 2048
 
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j2lte/dtb.img
 
-TARGET_KERNEL_CONFIG := cm_j2lteswa_00_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/j2lte
+#TARGET_KERNEL_CONFIG := cm_j2lteswa_00_defconfig
+#TARGET_KERNEL_SOURCE := kernel/samsung/j2lte
+PRODUCT_COPY_FILES += device/samsung/j2lte/prebuilt_kernel:kernel
 
 #Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x360000000
